b0c906dac940aaeb7e844ba85e33ac87d1893149
Jakub Wieczorek <fawek@fawek.net>
2012-02-27 23:33:03 +0100

    Initial commit

---
+++ README.markdown
@@ -0,0 +1,32 @@
+Code Painter
+============
+
+Code Painter is a JavaScript beautifier that instead of asking you to manually specify the desired  formatting style, can infer it from a code sample provided by you. This could, for instance, be a code snippet from the same project that your new code is supposed to be integrated with.
+
+It uses the excellent [Esprima parser](http://esprima.org/) by [Ariya Hidayat](http://ariya.ofilabs.com/) (thanks!).
+
+The name is inspired by Word's Format Painter, which does a similar job for rich text.
+
+Usage
+-----
+
+For now, it can only be used as a command-line tool but a Web version is in the works.
+
+> ./bin/codepaint -i input.js -s sample.js -o output.js
+
+transforms the input.js file using formatting style from sample.js and writes the output to output.js
+
+*-i* and *-o* can both be ommitted, in that case standard I/O streams will be used.
+
+> ./bin/codepaint -s sample.js < input.js > output.js
+
+The style can still be specified manually with a JSON string as the *--style* argument:
+
+> ./bin/codepaint --style '{ "QuoteType": "double" }' < input.js > output.js
+
+Supported style properties
+--------------------------
+
+1.  **QuoteType**: *single*, *double*
+
+    Specifies what kind of quoting you would like to use for string literals. Adds proper escaping when necessary, obviously.
---
+++ codepainter.js
@@ -0,0 +1,69 @@
+const Pipe = require('./lib/Pipe'),
+      rules = require('./lib/rules'),
+      Serializer = require('./lib/Serializer'),
+      Tokenizer = require('./lib/Tokenizer');
+
+module.exports.infer = function (sample, callback) {
+    var left = rules.length,
+        style = {},
+        tokenizer = new Tokenizer();
+        
+    sample.pipe(tokenizer);
+        
+    if (rules.length > 0) {
+        var left = rules.length;
+        for (var i = 0; i < rules.length; i++) {
+            var rule = rules[i];
+            rule.infer(tokenizer, function (error, value) {
+                style[rule.name] = value;
+                left--;
+                if (left === 0)
+                    callback(null, style);
+            });
+        }
+    } else {
+        callback(null, style);
+    }
+
+    sample.resume();
+};
+
+module.exports.transform = function (input, style, output, callback) {
+    var enabledRules = [],
+        left = rules.length,
+        tokenizer = new Tokenizer(),
+        serializer = new Serializer(),
+        streams = [];
+       
+    for (var i = 0; i < rules.length; i++) {
+        var rule = rules[i];
+        if (typeof style[rule.name] !== 'undefined')
+            enabledRules.push(rule);
+    }
+       
+    input.pipe(tokenizer);
+    serializer.pipe(output);
+        
+    if (enabledRules.length > 0) {
+        var streams = [];
+        streams.push(tokenizer);
+        
+        for (var i = 0; i < enabledRules.length - 1; i++)
+            streams.push(new Pipe());
+        streams.push(serializer);
+    
+        for (var i = 0; i < enabledRules.length; i++) {
+            var rule = enabledRules[i];
+            rule.transform(streams[i], style[rule.name], streams[i + 1], function (error) {
+            });
+        }
+    } else {
+        tokenizer.pipe(serializer);
+    }
+    
+    serializer.on('end', function () {
+        callback(null);
+    });
+    
+    input.resume();
+};
